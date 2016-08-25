8da5f21498a16c8d8629160e214f8de8b4d2cfa1
Zpeugh <zach.peugh@spidasoftware.com>
2016-08-25 18:11:58 -0400

    Adding speech recording maybe

--- analyze_tone.py
+++ analyze_tone.py
@@ -1,6 +1,9 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import pyaudio
+import wave
 
 
 tone_analyzer = ToneAnalyzerV3(
@@ -10,30 +13,81 @@ tone_analyzer = ToneAnalyzerV3(
 
 sentence = raw_input("Enter a sentence to have analyzed: ")
 
-
 json_response = tone_analyzer.tone(text=sentence)
 
-emotion = json_response["document_tone"]["tone_categories"][0]
-language = json_response["document_tone"]["tone_categories"][1]
-social = json_response["document_tone"]["tone_categories"][2]
+categories = json_response["document_tone"]["tone_categories"]
+# language = json_response["document_tone"]["tone_categories"][1]
+# social = json_response["document_tone"]["tone_categories"][2]
 
-print(emotion)
 
-def make_pie_charts(json):
-    category_name = json["category_name"]
-    print("Category: " + category_name)
-    labels = []
-    scores = []
-    for obj in json["tones"]:
-        labels.append(obj["tone_name"])
-        scores.append(obj["score"])
+def make_pie_charts(json_array):
+    grid = GridSpec(1,3)
+    color_array = ['steelblue', 'crimson', 'darkseagreen', 'darkcyan', 'orange']
+    pos = 0
+    color_pos = 0
+    for category in json_array:
+        category_name = category["category_name"]
+        print("Category: " + category_name)
+        labels = []
+        scores = []
+        colors = []
+        for obj in category["tones"]:
+            labels.append(obj["tone_name"])
+            scores.append(obj["score"])
+            colors.append(color_array[color_pos])
+            color_pos += 1
+        color_pos = 0
 
-    plt.pie(scores, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-    plt.axis('equal')
-    plt.title(category_name)
+        plt.subplot(grid[0,pos], aspect=1)
+        plt.pie(scores, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+        plt.title(category_name)
+        pos += 1
     plt.show()
 
+def record_audio():
 
-make_pie_charts(emotion)
-make_pie_charts(language)
-make_pie_charts(social)
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 5
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+
+    print "* recording"
+    for i in range(0, 44100 / chunk * RECORD_SECONDS):
+        data = stream.read(chunk)
+
+        # check for silence here by comparing the level with 0 (or some threshold) for
+        # the contents of data.
+        # then write data or not to a file
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print(data)
+    return data
+
+def record_to_file(path):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = record_audio()
+    data = pack('<' + ('h'*len(data)), *data)
+
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+
+data = record_audio()
+record_to_file("sentence.wav", data)
+make_pie_charts(categories)
